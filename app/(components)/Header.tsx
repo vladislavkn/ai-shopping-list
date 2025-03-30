0d9b2134d6d0d90462fee164c44ffd5147be14c3
Vladislav Knyshov <inova136@gmail.com>
2025-03-30 21:50:56 +0200

    feat: Add theme toggle functionality and update global styles

--- app/(components)/Header.tsx
+++ app/(components)/Header.tsx
@@ -1,17 +1,21 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import ModeToggle from "./ModeToggle";
 
 export default function Header() {
     return <header className="flex items-center justify-between p-4">
-        <div className="flex items-center gap-2">
+        <div className="flex items-center gap-1">
             <Image width={48} height={48} src="/ai-shopping-list-log.webp" alt="Logo of ai shopping list: a happy cat with a carrot" />
-            <h1 className="font-bold text-xl">AI shopping list</h1>
+            <h1 className="font-bold text-lg">AI shopping list</h1>
         </div>
-        <Button asChild variant="link">
-            <a href="#">
-                <Image width={24} height={24} src="/github-mark.svg" alt="Github logo" />
-                Github
-            </a>
-        </Button>
+        <div className="flex items-center gap-1">
+            <Button asChild variant="link" size="sm">
+                <a href="#">
+                    <Image width={24} height={24} src="/github-mark.svg" alt="Github logo" className="dark:invert" />
+                    Github
+                </a>
+            </Button>
+            <ModeToggle />
+        </div>
     </header>
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import "./globals.css";
 import Header from "./(components)/Header";
+import ThemeProvider from "./(components)/ThemeProvider";
 
 const robotoSans = Roboto({
   weight: ['400', '700'],
@@ -20,12 +21,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${robotoSans.className} antialiased`}
       >
-        <Header />
-        <div className="2xl:container px-4">{children}</div>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Header />
+          <div className="2xl:container px-4">{children}</div>
+        </ThemeProvider>
       </body>
     </html>
   );
